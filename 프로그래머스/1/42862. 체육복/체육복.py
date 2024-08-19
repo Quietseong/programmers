9949132d012e956691ad9d20e3f4e93cf4b8586f
Quietseong <105484422+Quietseong@users.noreply.github.com>
2024-08-19 20:23:44 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,26 +1,20 @@
-# def solution(n, lost, reserve):
-#     lost = set(lost) - set(reserve)
-#     reserve = set(reserve) - set(lost)
-
-#     for i in reserve:
-#         if i-1 in lost:
-#             lost -= {i-1, }
-#         else:
-#             lost -= {i+1, }
-#     return n - len(lost)
+#+test case
+#lost = [2,4]
+#reserve = [1,2,5]
+#n=5
 
 def solution(n, lost, reserve):
-    lost_set = set(lost)
-    reserve_set = set(reserve)
-    reserve_and_lost = lost_set & reserve_set
+    lost_set = set(lost) #2,4
+    reserve_set = set(reserve) #1,2,5
+    dup = lost_set & reserve_set #2
 
-    lost_set -= reserve_and_lost
-    reserve_set -= reserve_and_lost
+    lost_set -= dup #4
+    reserve_set -= dup #1,5
 
-    for i in reserve_set:
-        if i - 1 in lost_set:
-            lost_set -= {i - 1, }
-        elif i + 1 in lost_set:
-            lost_set -= {i + 1, }
+    for i in reserve_set: #여벌 전 후 idx의 도난 체육복 체크
+        if i-1 in lost_set: #4
+            lost_set -= {i-1, } #remove 4, remove():KeyErr
+        elif i+1 in lost_set: #i-1에 없는 경우
+            lost_set -= {i+1, } #remove
 
-    return n - len(lost_set)    
+    return n - len(lost_set) #5-0=5
